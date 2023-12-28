fab6af43894fe1ff792d554f7d370106c58806b7
P-Chan <hello@0x50.io>
2023-12-28 10:52:23 +0900

    feat: next を追加

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  )
+}
+
+export default Layout
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+const Page = () => {
+  return <h1>next-storybook-svgr-sandbox</h1>
+}
+
+export default Page
